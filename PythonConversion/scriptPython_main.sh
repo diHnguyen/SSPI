1636ad3fe1c189bd9c9c06e1745b012816dd744f
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2024-10-02 16:54:30 +0100

    mainA1=1 done
    Double check N30 instances

--- PythonConversion/scriptPython_main.sh
+++ PythonConversion/scriptPython_main.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-for N in 40;
-do
-    for i in {9..10};
-    do
-        echo "Running N=$N d=$d i=$i "
-        gtimeout -k 5 3600 python main.py $N $i
-        # gtimeout -k 5 2s sleep $((i*10)) && echo "Hello"
-        echo "End of 3600 seconds"
-    done
-done
-
-for N in 50;
+for N in 30 40 50;
 do
     for i in {1..10};
     do
