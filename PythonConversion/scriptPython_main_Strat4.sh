71534742e1ecc296fbebb21a6408e501c2bdfe5b
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2024-10-30 08:59:05 +0000

    n=10000, Strat4

---
+++ PythonConversion/scriptPython_main_Strat4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for N in 30 40 50;
+do
+    for i in {1..10};
+    do
+        echo "Running N=$N d=$d i=$i "
+        gtimeout -k 5 3600 python main_Strat4.py $N $i
+        # gtimeout -k 5 2s sleep $((i*10)) && echo "Hello"
+        echo "End of 3600 seconds"
+    done
+done
