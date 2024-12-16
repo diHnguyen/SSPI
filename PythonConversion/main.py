739204e9580f12b980c90d54b44519d111840445
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2024-12-16 22:42:32 +0000

    delete files

--- PythonConversion/main.py
+++ PythonConversion/main.py
@@ -62,8 +62,8 @@ from functionGetCellInfo import getCellInfo
 # x_now = np.zeros(Len)
 # newCell = 2
 k=1
-A1=0
-A3=1 
+A1=0 #A1 = 0: Choose arc using worst cost. Else: Choose arc w largest M
+A3=1 #A3 = 0: Split a selected using SA if possible. Else: Split using mean base cost.
 
 # Calculate c values
 c = (cU_orig + cL_orig) / 2
