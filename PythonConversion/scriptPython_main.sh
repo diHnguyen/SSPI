804ac1c106966187cfa215a6267fe5319a0d7e7f
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2024-12-31 12:56:08 -0500

    write to iter -- "-1" in last iteration

--- PythonConversion/scriptPython_main.sh
+++ PythonConversion/scriptPython_main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for density in 15;
 do
-    for N in 50;
+    for N in 40;
     do
-        for i in {32..150};
+        for i in {1..50};
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main.py $N $i $density
