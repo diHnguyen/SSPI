4d3e14e65b91384d6a1676fde632b10783fa0785
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-01-29 19:16:21 +0000

    >60min, N39,40,50

--- PythonConversion/scriptPython_main_A2_1.sh
+++ PythonConversion/scriptPython_main_A2_1.sh
@@ -18,7 +18,7 @@ for density in 15;
 do
     for N in 50;
     do
-        for i in 18 26 30 46 73 93 98 110 113;
+        for i in 1 3 4 5 8 9 11 12 19 20;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main_A3_0.py $N $i $density
