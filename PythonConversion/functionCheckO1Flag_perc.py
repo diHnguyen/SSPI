720ce8b496527d504ebd49e53cf2749a6b66f748
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-03-27 16:33:49 +0000

    Prep for default reruns

---
+++ PythonConversion/functionCheckO1Flag_perc.py
@@ -0,0 +1,107 @@
+#Same as fuctionCheckO1Flag but uses delta1 as percentage instead
+import numpy as np
+import importlib
+import pandas as pd
+
+importlib.import_module("functionGetCellInfo")
+from functionGetCellInfo import getCellInfo
+importlib.import_module("functionGbound")
+from functionGbound import gx_bound
+
+def checkO1Flag(m,x,z,Len,O1Flag,delta1,newCell,edge,origin,destination,last_x,x_now,d, k,z_now,df_cell,df_constraints):
+    if not np.array_equal(last_x, x_now):
+        K_bar = list(range(newCell+1))
+        # print("checkO1Flag/ K_bar = ", K_bar)
+        last_x = x_now
+        # print("Running O1Flag", K_bar)
+        for k in K_bar:
+            # for index, row in df_cell[df_cell.CELL == k].iterrows():
+            #     print(k, "; ", row['LB'][0], "; ",row['UB'][0] )
+            # print("Before Concat")
+            # for index, row in df_constraints[(df_constraints.CELL == k)].iterrows():
+            #     print(k, "; ", np.where(row['Y']>0)[0], "; ",row['SP'] )
+            # print("k = ", k)
+            c_L, c_U, M, c, c_g, Y_k = getCellInfo(k, x_now, "c_g", d,  df_cell)
+            y, gx, SP, label, path = gx_bound(c, c_g, edge,origin,destination)
+            
+            # print("k ", k)
+            # print("c_L ", repr(c_L))
+            # print("c_U ", repr(c_U))
+            # arcs = np.where(y > 0)[0]
+            # print("y = ", repr(arcs))
+            # print("gx = ", gx)
+            # for e in arcs:
+            #     print(edge[e], " ", c[e], " ", c_g[e])
+            # print()
+            df_cell.at[k, 'g'] = gx
+            # t = np.array(df_cell.loc[df_cell.CELL==k, 'Y'])
+            # print("Y ", np.array(df_cell.loc[df_cell.CELL==k]['Y'][0]))
+            # print("y ", np.array(y))
+            # print("df_cell.loc[df_cell.CELL==k, 'Y'] ",df_cell.loc[df_cell.CELL==k, 'Y'])
+            # print("Before: ", df_cell)
+            df_cell.at[k,'Y'] = y
+            df_cell.at[k, 'PI'] = label
+            # print("After: ", df_cell)
+            # print("z_now[k]", z_now[k])
+            # print("gx ", gx)
+            # print("z_now = ", z_now[k])
+            if z_now[k] - gx > delta1*gx: #Used to be z_now[k] - gx > delta1
+                # print("Failing O1Flag")
+                # print('y ', np.where(y>0))
+                # con_num += 1
+#                 constraints_dict
+#                 # key = current_cell k
+#                 # Retrieve the record for the specified key
+#                 target_key = str(k)
+#                 target_record = constraints_dict[target_key]
+                
+#                 print(constraints_dict)
+                
+#                 # Access the constraints and values for the retrieved record
+#                 target_cons = target_record["cons"]
+#                 target_info = target_record["info"]
+
+#                 # Print information about the retrieved record
+#                 print(f"Record for cell: {target_key}")
+#                 print("Constraints:")
+#                 for constraint in target_cons:
+#                     print(f"  {constraint.ConstrName}")
+#                 print("Values:")
+#                 for key, value in target_info.items():
+#                     print(f"  {key}: {value}")
+                dtypes = {
+                    'CELL': int,
+                    'Y': object,
+                    'SP': object,  # Assuming 'Y' contains arrays
+                    'con': object
+                }
+                
+                new_row_data = {
+                                'CELL': [k],
+                                'Y': [y],
+                                'SP': [SP], #SP = newcell_RHS
+                                'con': m.addConstr(z[k] <= sum(d[i] * x[i] * y[i] for i in range(Len)) + SP)
+                            }
+                # con = m.addConstr(z[k] <= sum(d[i] * x[i] * y[i] for i in range(Len)) + SP)
+                # print(type(con))
+                df_new_row = pd.DataFrame(new_row_data, columns=dtypes.keys()).astype(dtypes)
+                # df_constraints.loc[con_num - 1] = [con_num, newCell, Y_k.tolist(), newCell_RHS]
+                # df_constraints = df_constraints.append(new_row_data, ignore_index=True)
+                
+                df_constraints = pd.concat([df_constraints,df_new_row], axis=0, ignore_index=True)
+                # df_constraints.loc[con_num] = [con_num, k, y.tolist(), SP]
+                #Example: m.addConstr(z[1] <= SP_init + sum(yy[i]*x[i]*d[i] for i in range(Len)))
+                # constr[con_num] = m.addConstr(m, z[k] <= sum(d[i] * y[i] * x[i] for i in range(1, Len + 1)) + SP)
+                # print("Len ", Len)
+                # print("d ", len(d))
+                O1Flag = False
+                
+            # print("After Concat - if any")
+            # for index, row in df_constraints[(df_constraints.CELL == k)].iterrows():
+            #     print(k, "; ", np.where(row['Y']>0)[0], "; ",row['SP'] )
+                # m.addConstr(z[k] <= sum(d[i] * y[i] * x[i] for i in range(Len)) + SP)
+                
+            # print(k, "After Concat")
+            # for index, row in df_constraints[(df_constraints.CELL == k)].iterrows():
+            #     print(k, "; ", np.where(np.array(row['Y'])>0)[0], "; ",row['SP'] )
+    return O1Flag, K_bar, df_cell, df_constraints
