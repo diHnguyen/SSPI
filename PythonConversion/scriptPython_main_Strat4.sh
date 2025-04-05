b7780a656236d8e970db039654c8f489fa0be024
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-04-05 13:13:56 +0100

    <=60min, all ins

--- PythonConversion/scriptPython_main_Strat4.sh
+++ PythonConversion/scriptPython_main_Strat4.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-# For <5 minutes:
+#<5min
+#N30: 1 2 5 6 7 8 9 10 11 12
+#N40: 1 3 5 6 7 9 10 11 12 13
+#N50: 2 6 7 10 13 14 15 16 17 21
 
-# N30: 1 2 5 6 7 8 9 10 11 12 xxx 
-# N40: 1 3 5 6 7 9 10 11 12 13 xxx 
-# N50: 2 6 7 10 12 13 14 15 16 17 xxx
-# For 5-60 minutes:
+#5-60min
+#N30: 4 20 83 89 107 166 172 174 200 241
+#N40: 4 8 16 20 27 28 33 36 37 48
+#N50: 18 26 30 46 73 93 98 110 113 122
 
-# N30: 4 20 37 64 83 107 150 174 258 277 xxx
-# N40: 4 8 24 27 37 48 64 68 77 82 xxx
-# N50: 3 8 26 27 28 39 41 46 54 73 xxx
-# For >60 minutes: 
-
-# N30: 3 35 36 52 63 282 319 334 546 549 
-# N40: 2 14 18 22 29 38 39 40 56 76
-# N50: 1 4 5 9 11 20 23 32 34 38 
+#>60min
+#N30: 3 35 36 37 52 63 64 150 282 319 
+#N40: 2 14 18 22 24 38 39 40 56 76 
+#N50: 1 3 4 5 8 9 11 12 19 20 
 
 
 for n in 100; 
@@ -22,7 +21,7 @@ do
     do
         for N in 30;
         do
-            for i in 1 2 5 6 7 8 9 10 11 12 ;
+            for i in 1 2 5 6 7 8 9 10 11 12;
             do
                 echo "Running N=$N density=$density i=$i "
                 gtimeout -k 5 3600 python main_Strat4.py $N $i $density $n
@@ -35,7 +34,7 @@ do
     do
         for N in 40;
         do
-            for i in 1 3 5 6 7 9 10 11 12 13 ;
+            for i in 1 3 5 6 7 9 10 11 12 13;
             do
                 echo "Running N=$N density=$density i=$i "
                 gtimeout -k 5 3600 python main_Strat4.py $N $i $density $n
@@ -48,7 +47,7 @@ do
     do
         for N in 50;
         do
-            for i in 2 6 7 10 12 13 14 15 16 17 ;
+            for i in 2 6 7 10 13 14 15 16 17 21;
             do
                 echo "Running N=$N density=$density i=$i "
                 gtimeout -k 5 3600 python main_Strat4.py $N $i $density $n
