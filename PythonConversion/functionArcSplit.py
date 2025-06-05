4915181e03965ff0478a3acab94b81cde18d898c
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-06-05 13:14:24 +0100

    Need to get warm-start sol + implement alt bound

--- PythonConversion/functionArcSplit.py
+++ PythonConversion/functionArcSplit.py
@@ -1,36 +1,71 @@
 import importlib
 importlib.import_module("functionGbound")
 from functionGbound import gx_bound
-def arcSplit(x_now, arc_split, k, c_L, c_U, M, y, label,edge,origin,destination,d,Len,A3):
+import numpy as np
+def arcSplit(x_now, arc_split, k, c_L, c_U, M, y, label,edge,origin,destination,d,Len,A3,calls_SASplit,actual_SASplit):
     # global A1, A2, A3, A4, A5, edge, destination, d
     # global K_bar, K_newly_added, d, df_cell
+    # print("c_L ", c_L)
+    # sp = np.where(y > 0.1)[0]
+    # print("\nshortest path = ",sp)
+    # print("edges ", edge[sp,:])
+    # print("labels ", df_label, " " , len(df_label))
+    # print("sp labels ", label[label.node.isin(edge[sp,1])]['label'])
 
+    # for i in label.node:
+    #     print(label[label.node==i])
+    # print(edge[sp,:])
     c = (c_L + c_U) / 2
+    # print("costs ", c[sp])
+    # c_g = c + d*x_now
+    # print("c_g[sp] = ", c_g[sp])
     mean_split = True
     i = edge[arc_split][0]
     j = edge[arc_split][1]
-
+    # print("selected arc ", arc_split, "; c_L ", c_L[arc_split], "; c_U ", c_U[arc_split])
+    # print("Arc (i,j) = (", i,  ", " ,j,")")
+    label_i = np.array(label[label.node==i]['label'])[0]
+    label_j = np.array(label[label.node==j]['label'])[0]
+    # print("label[i] ", label_i, "; label[j] ", label_j)
+    # print()
+    # print("label[3] ", label[3])
+    # d_x = np.array([x*y for x,y in zip(d,x_now)])
+    # print("d[sp] ", d_x[sp])
+    # print("d[arc_split] * x_now[arc_split]", d[arc_split] * x_now[arc_split])
     if A3 == 0:  # 0=Split using SA if possible
+        calls_SASplit = calls_SASplit+1
         into_j = [index for index, edge in enumerate(edge) if edge[1] == j and edge[0] != i]
+        # print("into_j ", into_j)
+        # print(edge[into_j])
         temp_min = 1e6
         to_compare_arc = 0
-
         if y[arc_split] > 0.9:  # arc_split in y -- looking for labels/costs to replace (i, j)
             for arc_index in into_j:
+                # print("considering arc ", edge[arc_index])
                 out_k = edge[arc_index][0]
-                temp_c = (c[arc_index] + d[arc_index] * x_now[arc_index]) + label[out_k]
-
+                # print("out_k ", out_k)
+                if len(np.array(label[label.node==out_k]['label'])) > 0:
+                    label_k = np.array(label[label.node==out_k]['label'])[0]
+                else:
+                    label_k = 1e6
+                # print("label_k ", label_k)
+                # print("c[arc_index] ", c[arc_index])
+                temp_c = (c[arc_index] + d[arc_index] * x_now[arc_index]) + label_k#label[out_k]
+                # print("temp_c ", temp_c)
+                # print("to_compare_arc ", to_compare_arc)
+                # print("d[arc_index] * x_now[arc_index] ", d[arc_index] * x_now[arc_index])
                 if temp_min > temp_c:
                     temp_min = temp_c
                     to_compare_arc = arc_index
-
-            max_current_label = (c_U[arc_split] + d[arc_split] * x_now[arc_split]) + label[i]
-
+            
+            
+            max_current_label = (c_U[arc_split] + d[arc_split] * x_now[arc_split]) + label_i #label[i]
+            # print("max_current_label ", max_current_label)
             if temp_min < max_current_label - 0.0001:
                 ΔU = max_current_label - temp_min
                 ΔL = M[arc_split] - ΔU
                 mean_split = False
-
+        
         else:  # arc_split not in y -- check directly with labels to see when (i, j) is on the shortest path
             c2 = c.copy()
             c2[arc_split] = c_L[arc_split]
@@ -41,16 +76,19 @@ def arcSplit(x_now, arc_split, k, c_L, c_U, M, y, label,edge,origin,destination,
             y2_index = [i for i, val in enumerate(y2) if val > 0.9]
             y_cost_c2 = sum(y[i] * (c2[i] + d[i] * x_now[i]) for i in range(Len))
             y2_cost_c2 = sum(y2[i] * (c2[i] + d[i] * x_now[i]) for i in range(Len))
-
+            # print(y_index, "y_cost_c2 ", y_cost_c2)
+            # print(y2_index,"y2_cost_c2 ", y2_cost_c2)
             ΔL = y_cost_c2 - y2_cost_c2
-
+            # print("ΔL ", ΔL)
             if (ΔL > 0.0001) and (ΔL < M[arc_split] - 0.0001):
                 mean_split = False
                 ΔU = M[arc_split] - ΔL
-
+    if mean_split == False:
+        actual_SASplit = actual_SASplit+1
     if A3 == 1 or mean_split == True:  # 1=Split at mean base cost - we can always do so
         # print(M[arc_split])
         ΔL = M[arc_split] / 2
         ΔU = ΔL
-
-    return ΔL, ΔU
+    # print(k, "cL ", c_L[arc_split], "; cU ", c_U[arc_split])
+    # print(k, "ΔL ",ΔL,"; ΔU ", ΔU)
+    return ΔL, ΔU,calls_SASplit,actual_SASplit
--- PythonConversion/functionPartition_SA.py
+++ PythonConversion/functionPartition_SA.py
@@ -7,37 +7,72 @@ importlib.import_module("functionArcSplit")
 from functionArcSplit import arcSplit
 importlib.import_module("functionGbound")
 from functionGbound import gx_bound
+import time
 
-def Partition(x_now, newCell, k, p, c_L, c_U, M, y,d,edge,origin,destination,Len,A1,A3,df_cell, K_newly_added):
+def Partition(x_now, newCell, k, p, c_L, c_U, M, y,d,edge,origin,destination,Len,A1,A3,df_cell, K_newly_added,calls_SASplit,actual_SASplit, SA_time):
     # global K_bar, K_newly_added, d, df_cell
     # global A1, A2, A3, A4, A5
     # print("\t",k,": Partitioning (inside function)")
     # print("\tNewCell = ", newCell)
     # Selecting the arc to split
     # print("M ", M)
+    # print(0, " cL = ", c_L)
+    
+    now = time.time()
+    # print("now ", now)
     arc_split = selectArc(x_now, c_L, c_U, M, y,d,edge,origin,destination,A1)
-    # print("arc_split ", arc_split)
+    # print("time ", time.time())
+    SA_time = SA_time + (time.time()-now)
+    # print("SA_time ", SA_time)
+    # print("arc_split ", arc_split, edge[arc_split, :])
+    # print("pre slit cL cU", c_L[arc_split], " ", c_U[arc_split])
     label = df_cell.at[k, "PI"]
-    print("label ", type(label))
+    
     # Calculate ΔL and ΔU
-    ΔL, ΔU = arcSplit(x_now, arc_split, k, c_L, c_U, M, y, label,edge,origin,destination,d,Len,A3)
-
+    # c = (c_L + c_U)/2
+    # c_g = c + d * x_now
+    # y1, _, _, label1, path1 = gx_bound(c, c_g, edge, origin,destination)
+    # print("path1 = ", path1)
+    # print("labels = ", np.array(label1)[np.array(path1)])
+    # sp = np.array([0, 7, 20, 2, 26, 29])
+    # y1_arcs = np.where(y1 > 0.1)[0]
+    # print("c[sp] = ", c[y1_arcs])
+    # print("c_g[sp] = ", c_g[y1_arcs])
+    # print("y1 = ", y1_arcs)
+    # print("label1[26] ", label1[26])
+    # print("label1[29] ", label1[29])
+    # print("label1[3] ", label1[3])
+    # print("edge[20] ", edge[20])
+    # print("c[116] ", c[116])
+    # print("c[20] ", c[20])
+    # print("c_g[116] = ", c_g[116])
+    # print("c_g[20] = ", c_g[20])
+    # print("x[116] ", x_now[116])
+    # print("x[20] ", x_now[20])
+    # print("d[116] ", d[116])
+    # print("d[20] ", d[20])
+    ΔL, ΔU,calls_SASplit,actual_SASplit = arcSplit(x_now, arc_split, k, c_L, c_U, M, y, label,edge,origin,destination,d,Len,A3,calls_SASplit,actual_SASplit)
+    
     # Create info for the new cell K+1
     cL_newCell = np.copy(c_L)
     cL_newCell[arc_split] += ΔL
     cU_k = np.copy(c_U)
     cU_k[arc_split] = cU_k[arc_split] - ΔU
+    # print("post slit k+1 cL cU", cL_newCell[arc_split], " ", c_U[arc_split])
+    # print("post slit k cL cU", c_L[arc_split], " ", cU_k[arc_split])
     # print("cU_k = ", cU_k)
     # print("2.2")
     # print(df_cell)
     cL_avg = (c_L + cU_k) / 2
     cU_avg = (cL_newCell + c_U) / 2
     yL, gL, SP_L, label_L, path_L = gx_bound(cL_avg, cL_avg + d * x_now, edge, origin,destination)
-    # print("yL = ", yL)
+    
+    # print("yL = ", np.where(yL>0.1)[0], " cost ", gL)
     # print("cL_avg = ", cL_avg)
     # print("cL_avg + d * x_now = ", cL_avg + d * x_now)
     yU, gU, SP_U, label_U, path_U = gx_bound(cU_avg, cU_avg + d * x_now, edge, origin,destination)
     # print("label_L ",label_L)
+    # print("yU = ", np.where(yU>0.1)[0], " cost ", gU)
 
     current_p = df_cell.at[k, "PROB"]
     # print("current_p ", current_p)
@@ -77,6 +112,7 @@ def Partition(x_now, newCell, k, p, c_L, c_U, M, y,d,edge,origin,destination,Len
     # print("NEW ROW")
 #     print(pd.DataFrame(new_row))
     df_cell = pd.concat([df_cell,df_new_row], axis=0, ignore_index=True)
+    # print(df_cell)
     # print("2.3")
     # print(df_cell)
     # print("k = ", k)
@@ -109,4 +145,4 @@ def Partition(x_now, newCell, k, p, c_L, c_U, M, y,d,edge,origin,destination,Len
     ΔU /= 2
     # print("Inside Partition")
     # print(df_cell)
-    return ΔL, ΔU, arc_split, yL, yU, gL, gU, SP_L, SP_U,df_cell
+    return ΔL, ΔU, arc_split, yL, yU, gL, gU, SP_L, SP_U,df_cell,calls_SASplit,actual_SASplit, SA_time
