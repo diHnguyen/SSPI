9fb312f331b99227efb2888a8d311e9455d9c750
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-06-13 17:52:53 +0100

    all results done

--- PythonConversion/scriptPython_main_singleCut_combined_noAltBound_noWarmStart.sh
+++ PythonConversion/scriptPython_main_singleCut_combined_noAltBound_noWarmStart.sh
@@ -19,7 +19,7 @@ for density in 15;
 do
     for N in 30;
     do
-        for i in 1 2 5 6 7 8 9 10 11 12 ;
+        for i in 3 35 36 37 52 63 64 150 282 319;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main_singleCut_combined_noAltBound_noWarmStart.py $N $i $density
@@ -29,7 +29,7 @@ do
     done
     for N in 40;
     do
-        for i in 1 3 5 6 7 9 10 11 12 13 ;
+        for i in 2 14 18 22 24 38 39 40 56 76;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main_singleCut_combined_noAltBound_noWarmStart.py $N $i $density
@@ -39,38 +39,7 @@ do
     done
     for N in 50;
     do
-        for i in 2 6 7 10 13 14 15 16 17 21;
-        do
-            echo "Running N=$N density=$density i=$i "
-            gtimeout -k 5 3600 python main_singleCut_combined_noAltBound_noWarmStart.py $N $i $density
-            # gtimeout -k 5 2s sleep $((i*10)) && echo "Hello"
-            echo "End of 3600 seconds"
-        done
-    done
-
-    for N in 30;
-    do
-        for i in 4 20 83 89 107 166 172 174 200 241 ;
-        do
-            echo "Running N=$N density=$density i=$i "
-            gtimeout -k 5 3600 python main_singleCut_combined_noAltBound_noWarmStart.py $N $i $density
-            # gtimeout -k 5 2s sleep $((i*10)) && echo "Hello"
-            echo "End of 3600 seconds"
-        done
-    done
-    for N in 40;
-    do
-        for i in 4 8 16 20 27 28 33 36 37 48 ;
-        do
-            echo "Running N=$N density=$density i=$i "
-            gtimeout -k 5 3600 python main_singleCut_combined_noAltBound_noWarmStart.py $N $i $density
-            # gtimeout -k 5 2s sleep $((i*10)) && echo "Hello"
-            echo "End of 3600 seconds"
-        done
-    done
-    for N in 50;
-    do
-        for i in 18 26 30 46 73 93 98 110 113 122;
+        for i in 1 3 4 5 8 9 11 12 19 20;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main_singleCut_combined_noAltBound_noWarmStart.py $N $i $density
