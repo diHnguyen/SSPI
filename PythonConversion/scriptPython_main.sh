02d32dc070826e0d4307c9f0ba8cdc47fe49f633
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-08-06 20:46:28 -0400

    Update scriptPython_main.sh

--- PythonConversion/scriptPython_main.sh
+++ PythonConversion/scriptPython_main.sh
@@ -19,7 +19,7 @@ for density in 15;
 do
     for N in 30;
     do
-        for i in 1 2 5 6 7 8 9 10 11 12;
+        for i in ;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main.py $N $i $density
@@ -29,7 +29,7 @@ do
     done
     for N in 40;
     do
-        for i in 1 3 5 6 7 9 10 11 12 13  ;
+        for i in ;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main.py $N $i $density
@@ -39,7 +39,7 @@ do
     done
     for N in 50;
     do
-        for i in 2 6 7 10 13 14 15 16 17 21;
+        for i in 1 4 5 9 11 20;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main.py $N $i $density
