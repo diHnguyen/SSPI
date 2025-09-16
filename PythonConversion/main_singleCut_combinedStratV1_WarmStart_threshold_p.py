68a0df385aa117fbfde4cabd49f484253698b3fc
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-09-16 13:54:59 +0100

    Update main_singleCut_combinedStratV1_WarmStart_threshold_p.py

--- PythonConversion/main_singleCut_combinedStratV1_WarmStart_threshold_p.py
+++ PythonConversion/main_singleCut_combinedStratV1_WarmStart_threshold_p.py
@@ -303,7 +303,7 @@ while not terminate_cond:
             #     sys.out()
             if runningTest == False:
                 if printIters == True:
-                    with open(directory+'Dec2024_Output/Iter/main_singleCut_combinedStratV1_WarmStart_threshold_p'+density+'_'+testSet+'_'+sys.argv[2]+'.txt','a') as the_file:
+                    with open(directory+'Dec2024_Output/Iter/main_singleCut_combinedStratV1_WarmStart_threshold_p_'+density+'_'+testSet+'_'+sys.argv[2]+'.txt','a') as the_file:
                         the_file.write("I;"+str(iter)+";"+str(cur_time)+';'+str(b)+";"+str(MP_obj)+";"+str(LB_global)+";"+str(x_index)+";"+str(len(K_bar))+";"+str(newCell+1)+";"+str(calls_SASplit)+";"+str(actual_SASplit)+";"+str(SA_time)+"\n")
 
         O1Flag = True
@@ -490,7 +490,7 @@ if runningTest == True:
     # with open(directory+'./Dec2024_Output/test_main_d20_'+testSet+'_'+sys.argv[2]+'.txt','a') as the_file:
         # the_file.write("-1;"+str(cur_time)+';'+str(b)+";"+str(MP_obj)+";"+str(x_index)+";"+str(newCell+1)+"\n")
 else:
-    with open(directory+'./Dec2024_Output/'+'main_singleCut_combinedStratV1_WarmStart_threshold_p'+density+'_'+testSet+'_'+sys.argv[2]+'.txt','a') as the_file:
+    with open(directory+'./Dec2024_Output/'+'main_singleCut_combinedStratV1_WarmStart_threshold_p_'+density+'_'+testSet+'_'+sys.argv[2]+'.txt','a') as the_file:
         # the_file.write("-1;"+str(total_time)+';'+str(b)+";"+str(MP_obj)+";"+str(x_index)+";"+str(newCell+1)+"\n")
         the_file.write("C;"+str(iter)+";"+str(total_time)+';'+str(b)+";"+str(MP_obj)+";"+str(LB_global)+";"+str(x_index)+";"+str(len(K_bar))+";"+str(newCell+1)+";"+str(calls_SASplit)+";"+str(actual_SASplit)+";"+str(SA_time)+"\n")
     with open(directory+'Dec2024_Output/Iter/main_singleCut_combinedStratV1_WarmStart_threshold_p_'+density+'_'+testSet+'_'+sys.argv[2]+'.txt','a') as the_file:
