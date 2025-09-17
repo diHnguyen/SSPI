3cc24c4ad833cd574ed39f66b076438c6b53c29a
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-09-17 12:28:39 +0100

    Update scriptPython_main_singleCut_combinedStratV1_WarmStart_threshold_p.sh

--- PythonConversion/scriptPython_main_singleCut_combinedStratV1_WarmStart_threshold_p.sh
+++ PythonConversion/scriptPython_main_singleCut_combinedStratV1_WarmStart_threshold_p.sh
@@ -28,7 +28,7 @@ do
     done
     for N in 40;
     do
-        for i in  64 68 ;
+        for i in 4 8 16 27 33 36 37 48 64 68 2 14 18 22 24 38 39 40 56 76 ;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main_singleCut_combinedStratV1_WarmStart_threshold_p.py $N $i $density
