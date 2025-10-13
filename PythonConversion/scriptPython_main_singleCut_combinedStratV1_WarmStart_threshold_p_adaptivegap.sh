9be809308036fc4761b5483fa0a5d316c224d8f5
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-10-13 10:14:34 +0100

    Rename .py file

--- PythonConversion/scriptPython_main_singleCut_combinedStratV1_WarmStart_threshold_p_adaptivegap.sh
+++ PythonConversion/scriptPython_main_singleCut_combinedStratV1_WarmStart_threshold_p_adaptivegap.sh
@@ -21,27 +21,27 @@ do
         for i in ;
         do
             echo "Running N=$N density=$density i=$i "
-            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_WarmStart_threshold_p_adaptivegap.py $N $i $density
+            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_threshold_p_adaptivegap.py $N $i $density
             # gtimeout -k 5 2s sleep $((i*10)) && echo "Hello"
             echo "End of 3600 seconds"
         done
     done
     for N in 40;
     do
-        for i in ;
+        for i in 1 3 5 6 7 9 10 11 12 13  4 8 16 27 33 36 37 48 64 68 2 14 18 22 24 38 39 40 56 76;
         do
             echo "Running N=$N density=$density i=$i "
-            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_WarmStart_threshold_p_adaptivegap.py $N $i $density
+            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_threshold_p_adaptivegap.py $N $i $density
             # gtimeout -k 5 2s sleep $((i*10)) && echo "Hello"
             echo "End of 3600 seconds"
         done
     done
     for N in 50;
     do
-        for i in  1 3 4 5 8 9 11 12 19 20;
+        for i in  ;
         do
             echo "Running N=$N density=$density i=$i "
-            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_WarmStart_threshold_p.py $N $i $density
+            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_threshold_p_adaptivegap.py $N $i $density
             # gtimeout -k 5 2s sleep $((i*10)) && echo "Hello"
             echo "End of 3600 seconds"
         done
