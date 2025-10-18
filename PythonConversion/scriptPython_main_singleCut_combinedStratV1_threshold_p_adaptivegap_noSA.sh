188aa9804eb7937c43905da523f8bb33d7f6e533
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-10-18 13:57:48 +0100

    Rename .sh
    scriptPython_main_singleCut_combinedStratV1_threshold_p_adaptivegap.sh to
    scriptPython_main_singleCut_combinedStratV1_threshold_p_adaptivegap_noSA.sh

--- PythonConversion/scriptPython_main_singleCut_combinedStratV1_threshold_p_adaptivegap_noSA.sh
+++ PythonConversion/scriptPython_main_singleCut_combinedStratV1_threshold_p_adaptivegap_noSA.sh
@@ -21,7 +21,7 @@ do
         for i in 1 2 5 6 7 8 9 10 11 12 4 20 83 99 107 166 172 174 200 241 3 35 36 37 52 63 64 150 282 319 ;
         do
             echo "Running N=$N density=$density i=$i "
-            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_threshold_p_adaptivegap.py $N $i $density
+            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_threshold_p_adaptivegap_noSA.py $N $i $density
             # gtimeout -k 5 2s sleep $((i*10)) && echo "Hello"
             echo "End of 3600 seconds"
         done
@@ -31,7 +31,7 @@ do
         for i in ;
         do
             echo "Running N=$N density=$density i=$i "
-            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_threshold_p_adaptivegap.py $N $i $density
+            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_threshold_p_adaptivegap_noSA.py $N $i $density
             # gtimeout -k 5 2s sleep $((i*10)) && echo "Hello"
             echo "End of 3600 seconds"
         done
@@ -41,7 +41,7 @@ do
         for i in  2 6 7 10 13 14 15 16 17 21 18 26 46 73 93 98 110 113 122 124 1 3 4 5 8 9 11 12 19 20  ;
         do
             echo "Running N=$N density=$density i=$i "
-            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_threshold_p_adaptivegap.py $N $i $density
+            gtimeout -k 5 3600 python main_singleCut_combinedStratV1_threshold_p_adaptivegap_noSA.py $N $i $density
             # gtimeout -k 5 2s sleep $((i*10)) && echo "Hello"
             echo "End of 3600 seconds"
         done
