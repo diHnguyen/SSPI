1d2a5b7783ac982316254d34c4505abbf87fbcf0
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-11-05 18:22:58 +0000

    Update scriptPython_main_singleCut_combinedStratV1_WarmStart_threshold_p_onePartition.sh

--- PythonConversion/scriptPython_main_singleCut_combinedStratV1_WarmStart_threshold_p_onePartition.sh
+++ PythonConversion/scriptPython_main_singleCut_combinedStratV1_WarmStart_threshold_p_onePartition.sh
@@ -18,7 +18,7 @@ for density in 15;
 do
     for N in 30;
     do
-        for i in ;
+        for i in 1 2 5 6 7 8 9 10 11 12 4 20 83 99 107 166 172 174 200 241 3 35 36 37 52 63 64 150 282 319;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main_singleCut_combinedStratV1_WarmStart_threshold_p_onePartition.py $N $i $density
@@ -28,7 +28,7 @@ do
     done
     for N in 40;
     do
-        for i in 1 3 5 6 7 9 10 11 12 13  ;
+        for i in ;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main_singleCut_combinedStratV1_WarmStart_threshold_p_onePartition.py $N $i $density
@@ -38,7 +38,7 @@ do
     done
     for N in 50;
     do
-        for i in 2 6 7 10 13 14 15 16 17 21  ;
+        for i in ;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main_singleCut_combinedStratV1_WarmStart_threshold_p_onePartition.py $N $i $density
