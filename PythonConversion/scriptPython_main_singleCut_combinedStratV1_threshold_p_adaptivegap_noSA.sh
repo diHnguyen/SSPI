11da9c9fadfc65514a0f0d6560fa711a28484ac9
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-11-18 22:04:58 +0000

    Update scriptPython_main_singleCut_combinedStratV1_threshold_p_adaptivegap_noSA.sh

--- PythonConversion/scriptPython_main_singleCut_combinedStratV1_threshold_p_adaptivegap_noSA.sh
+++ PythonConversion/scriptPython_main_singleCut_combinedStratV1_threshold_p_adaptivegap_noSA.sh
@@ -28,7 +28,7 @@ do
     done
     for N in 40;
     do
-        for i in  ;
+        for i in  1 3 5 6 7 9 10 11 12 13 4 8 16 27 33 36 37 48 64 68 2 14 18 22 24 38 39 40 56 76;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main_singleCut_combinedStratV1_threshold_p_adaptivegap_noSA.py $N $i $density
@@ -38,7 +38,7 @@ do
     done
     for N in 50;
     do
-        for i in ;
+        for i in 2 6 7 10 13 14 15 16 17 21 18 26 46 73 93 98 110 113 122 124 1 3 4 5 8 9 11 12 19 20;
         do
             echo "Running N=$N density=$density i=$i "
             gtimeout -k 5 3600 python main_singleCut_combinedStratV1_threshold_p_adaptivegap_noSA.py $N $i $density
