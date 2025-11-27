37074889c4a09084fc55c3c7fd9985b27d5091cd
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2025-11-27 03:20:35 +0000

    Update main_singleCut_Times.py

--- PythonConversion/main_singleCut_Times.py
+++ PythonConversion/main_singleCut_Times.py
@@ -426,7 +426,7 @@ if runningTest == True:
     # with open(directory+'./Dec2024_Output/test_main_d20_'+testSet+'_'+sys.argv[2]+'.txt','a') as the_file:
         # the_file.write("-1;"+str(cur_time)+';'+str(b)+";"+str(MP_obj)+";"+str(x_index)+";"+str(newCell+1)+"\n")
 else:
-    with open(directory+'./Dec2024_Output/'+'main_singleCut_'+density+'_'+testSet+'_'+sys.argv[2]+'.txt','a') as the_file:
+    # with open(directory+'./Dec2024_Output/'+'main_singleCut_'+density+'_'+testSet+'_'+sys.argv[2]+'.txt','a') as the_file:
         # the_file.write("-1;"+str(total_time)+';'+str(b)+";"+str(MP_obj)+";"+str(x_index)+";"+str(newCell+1)+"\n")
         # the_file.write("C;"+str(iter)+";"+str(_end_opt)+";"+str(_end_par)+";"+str(total_time)+';'+str(b)+";"+str(MP_obj)+";"+str(LB_global)+";"+str(x_index)+";"+str(len(K_bar))+";"+str(newCell+1)+"\n")
     with open(directory+'Dec2024_Output/Times/main_singleCut_'+density+'_'+testSet+'_'+sys.argv[2]+'.txt','a') as the_file:
