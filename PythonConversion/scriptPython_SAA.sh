2f0163b3ca0c3b02e39d9005c4bce420f412f192
Di H. Nguyen <55147025+diHnguyen@users.noreply.github.com>
2026-05-26 01:33:08 +0100

    Update scriptPython_SAA.sh

--- PythonConversion/scriptPython_SAA.sh
+++ PythonConversion/scriptPython_SAA.sh
@@ -20,9 +20,9 @@ for density in 15;
 do
     for N in 30;
     do
-        for i in 99 166 172 200 241;
+        for i in 1 2 5 6 7 8 9 10 11 12 4 20 83 89 107 166 172 174 200 241 3 35 36 37 52 63 64 150 282 319;
         do
-            for num_cases in 1000 10000 100000;
+            for num_cases in 1000 10000;
             do
                 echo "Running N=$N density=$density i=$i "
                 gtimeout -k 5 3600 python SAA.py $N $i $density $num_cases
@@ -36,9 +36,9 @@ for density in 15;
 do
     for N in 40;
     do
-        for i in 16 33 36 ;
+        for i in 1 3 5 6 7 9 10 11 12 13 4 8 16 27 33 36 37 48 64 68 2 14 18 22 24 38 39 40 56 76;
         do
-            for num_cases in 1000 10000 100000;
+            for num_cases in 1000 10000 ;
             do
                 echo "Running N=$N density=$density i=$i "
                 gtimeout -k 5 3600 python SAA.py $N $i $density $num_cases
@@ -52,9 +52,9 @@ for density in 15;
 do
     for N in 50;
     do
-        for i in 21 18 93 98 110 113 122 124 19;
+        for i in 2 6 7 10 13 14 15 16 17 21 18 26 46 73 93 98 110 113 122 124 1 3 4 5 8 9 11 12 19 20;
         do
-            for num_cases in 1000 10000 100000;
+            for num_cases in 1000 10000 ;
             do
                 echo "Running N=$N density=$density i=$i "
                 gtimeout -k 5 3600 python SAA.py $N $i $density $num_cases
